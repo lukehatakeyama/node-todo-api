6aac33a02b2b7289a9a0db90a43baeb51eb02141
Luke Hatakeyama <luke.hatakeyama@gmail.com>
2018-05-23 17:50:28 -0700

    add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -22,11 +23,28 @@ app.post('/todos', (req, res) => {
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) =>{
-      res.send({todos});
-    }, (e) => {
-      res.status(400).send(e);
-    })
+  Todo.find().then((todos) =>{
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send()
+    }
+    return res.send({todo});
+  }, (e) => {
+    return res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
