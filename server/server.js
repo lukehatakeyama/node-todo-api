4381092bdf199f72de43a195c305acc21676b724
Luke Hatakeyama <luke.hatakeyama@gmail.com>
2018-05-25 16:40:09 -0700

    Setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require('./config/config');
 
 const _ = require('lodash');
 const express = require('express');
@@ -8,7 +9,7 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 var app = express();
 
